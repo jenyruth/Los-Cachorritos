0d8be1e395ce90986ec4c47bac1e6578975c2d85
johnel7 <nhelrojas00@gmail.com>
2019-01-05 12:56:39 -0500

    archivo header

---
+++ CPU-UNAMBA/application/views/layouts/header.php
@@ -0,0 +1,44 @@
+<header>
+  
+ 
+<nav class="navbar navbar-expand-lg navbar-light bg-info fixed-top">
+      <div class="container">
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    
+                  <a class="navbar-brand" href="#">
+                      <div class="row">
+                          <div class="col"><img alt="Brand" class=" img-responsivefeaturette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" src="<?PHP echo base_url('public/LOGO-UNAMBA.png')?>" data-holder-rendered="false" style="width: 155px; height: 39px;">
+                      
+                    </div>
+                      </div>
+                  </a>
+                </div>
+            </div>
+        </nav>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse menu" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active menucolor">
+                <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url();?>CPU">Inscripcion Onnile</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url();?>auth">Iniciar Sesion</a>
+              </li>
+
+            </ul>
+
+          </div>
+      </div>
+    </nav>
+   <!--Menu-->
+
+    </header>
+<main role="main">
