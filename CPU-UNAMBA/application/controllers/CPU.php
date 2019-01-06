7842bb8ee6cf163b5f175e0b77b1147d15fe0258
132090 <132090.rock@gmail.com>
2019-01-05 20:47:23 -0500

    programando en el controlador CPU 1

--- CPU-UNAMBA/application/controllers/CPU.php
+++ CPU-UNAMBA/application/controllers/CPU.php
@@ -1,1 +1,26 @@
+<?php
 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class CPU extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('Model_cpu');
+    }
+
+    public function index()
+    {
+	$this->load->view('layouts/head');
+        $this->load->view('layouts/header_1');
+        //$this->load->view('admin/estudiante'); 
+        /*$data['depar']= $this->Model_cpu->depar();
+       
+        $data['prov']= $this->Model_cpu->prov();
+        $data['dist']= $this->Model_cpu->dist();*/
+        $this->load->view("admin/estudiante");
+        $this->load->view('layouts/footer');
+        
+    }
+    
+}
