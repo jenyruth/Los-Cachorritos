9e25d1983f6f602dd5dd592fe024f8b454ac155f
132090 <132090.rock@gmail.com>
2019-01-05 20:42:26 -0500

    configurando la parte de ingreso a registro

--- CPU-UNAMBA/application/controllers/Inicio.php
+++ CPU-UNAMBA/application/controllers/Inicio.php
@@ -16,4 +16,29 @@ class Inicio extends CI_Controller {
         $this->load->view('layouts/conteiner');
         $this->load->view('layouts/footer');
     }
+    public function login(){
+        /*$num_rec = $this->input->post("num_rec");
+	//$res= $this->Model_Inic->login(sha1($username));
+        $res = $this->Model_Inic->login($num_rec);
+        
+        if(!$res){
+            redirect(base_url());
+        }else{
+            $data=array(
+              'num_rec'=>$res->num_rec,
+              'tipo_us'=>$res->tipo_us 
+            );
+            $this->session->set_userdata($data);
+            redirect(base_url('cpu'));
+        }*/
+        
+        //$this->load->view('admin/estudiante'); 
+        /*$data['depar']= $this->Model_cpu->depar();
+       
+        $data['prov']= $this->Model_cpu->prov();
+        $data['dist']= $this->Model_cpu->dist();*/
+        
+        $this->load->view('admin/login');
+        
+    }
 }
