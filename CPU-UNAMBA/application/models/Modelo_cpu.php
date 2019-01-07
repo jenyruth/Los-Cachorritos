119276a5fd34d26f72e6014b1feaab7dd841d880
132090 <132090.rock@gmail.com>
2019-01-07 18:15:12 -0500

    creando la funcion en el modelo cpu para los departamentos

--- CPU-UNAMBA/application/models/Modelo_cpu.php
+++ CPU-UNAMBA/application/models/Modelo_cpu.php
@@ -8,4 +8,8 @@ class Modelo_cpu extends CI_Model {
         $this->load->database();
     }
     
+    public function depar() {
+        $query=$this->db->query("SELECT * FROM departamentos");
+        return $query->result();
+    }
 }
