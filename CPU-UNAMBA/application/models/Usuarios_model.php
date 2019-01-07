f9f136266c4fbef279d9171de6965c63d351022a
johnel7 <nhelrojas00@gmail.com>
2019-01-07 18:07:46 -0500

    funcion getUsuario por ID

--- CPU-UNAMBA/application/models/Usuarios_model.php
+++ CPU-UNAMBA/application/models/Usuarios_model.php
@@ -12,3 +12,13 @@ class Usuarios_model extends CI_Model {
 		return $resultados->result();
     }
     
+    public function getUsuario($id){
+		$this->db->select("u.*,r.nombre as rol");
+		$this->db->from("usuarios u");
+		$this->db->join("roles r","u.rol_id = r.id");
+		$this->db->where("u.id",$id);
+		$this->db->where("u.estado","1");
+		$resultado = $this->db->get();
+		return $resultado->row();
+	}
+
