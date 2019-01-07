d35aa2028602273343512dbef994de3fce29a645
132090 <132090.rock@gmail.com>
2019-01-07 18:07:41 -0500

    comunicando mi modelo con la bd del proyecto

--- CPU-UNAMBA/application/models/Modelo_cpu.php
+++ CPU-UNAMBA/application/models/Modelo_cpu.php
@@ -1,8 +1,11 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+defined('BASEPATH') OR exit('No direct script access allowed');
 
+class Modelo_cpu extends CI_Model {
+    function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+    
+}
