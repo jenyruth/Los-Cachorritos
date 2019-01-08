bfd28e01d9972c1bd14feefeda5839742903e247
johnel7 <nhelrojas00@gmail.com>
2019-01-08 16:38:32 -0500

    subiendi el footer del lado del administrador

---
+++ CPU-UNAMBA/application/views/admin/layouts/footer.php
@@ -0,0 +1,29 @@
+<footer class="main-footer">
+            <div class="pull-right hidden-xs">
+                <b>Version</b> 3.4.0
+            </div>
+            <strong>Copyright &copy; 2018-2019 <a href="https://codemyn.blogspot.com/">By Johnel</a>.</strong> All rights
+            reserved.
+        </footer>
+    </div>
+    <!-- ./wrapper -->
+<!-- jQuery 3 -->
+<script src="<?php echo base_url();?>public/template/jquery/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="<?php echo base_url();?>public/template/bootstrap/js/bootstrap.min.js"></script>
+<!-- SlimScroll -->
+<script src="<?php echo base_url();?>public/template/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+
+<!-- FastClick -->
+<script src="<?php echo base_url();?>public/template/fastclick/lib/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo base_url();?>public/template/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?php echo base_url();?>public/template/dist/js/demo.js"></script>
+<script>
+$(document).ready(function () {
+$('.sidebar-menu').tree()
+})
+</script>
+</body>
+</html>
