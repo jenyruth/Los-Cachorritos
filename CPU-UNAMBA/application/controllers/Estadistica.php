574a98e94de3fb1ba6498309089447635ea6126d
132090 <132090.rock@gmail.com>
2019-01-08 18:27:01 -0500

    programando, el enlace de parte del controlador para el cuadro de estadiscticas

--- CPU-UNAMBA/application/controllers/Estadistica.php
+++ CPU-UNAMBA/application/controllers/Estadistica.php
@@ -1,8 +1,19 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+defined('BASEPATH') OR exit('No direct script access allowed');
 
+class Estadistica extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        //$this->load->model('Model_cpu');
+    }
+
+    public function index()
+    {
+	$this->load->view('admin/pages/cuadro1');
+        
+        
+    }
+    
+}
