bf0b39b06d3ee34f1819b4b539ead7785f9544d4
132090 <132090.rock@gmail.com>
2019-01-08 18:06:36 -0500

    en esta parte se modifica la ruta de enlace al inico del admin

--- CPU-UNAMBA/application/views/admin/layouts/header.php
+++ CPU-UNAMBA/application/views/admin/layouts/header.php
@@ -21,7 +21,7 @@
     <div class="wrapper">
         <header class="main-header">
             <!-- Logo -->
-            <a href="../../index2.html" class="logo">
+            <a href="<?php echo base_url();?>../CPU-UNAMBA/" class="logo">
                 <!-- mini logo for sidebar mini 50x50 pixels -->
                 <span class="logo-mini"><b>CP</b>U</span>
                 <!-- logo for regular state and mobile devices -->
