8be2334fa6e5fd68d73d559026eb0af1654af378
132090 <132090.rock@gmail.com>
2019-01-08 18:42:54 -0500

    tercer cuadro con lineas

--- CPU-UNAMBA/application/views/admin/pages/cuadro1.php
+++ CPU-UNAMBA/application/views/admin/pages/cuadro1.php
@@ -56,7 +56,32 @@
           <!-- /.box -->
 
         </div>
-        
+        <!-- /.col (LEFT) -->
+        <div class="col-md-6">
+          <!-- LINE CHART -->
+          <div class="box box-info">
+            <div class="box-header with-border">
+              <h3 class="box-title">Line Chart</h3>
+
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            <div class="box-body">
+              <div class="chart">
+                <canvas id="lineChart" style="height:250px"></canvas>
+              </div>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+
+          >
+
+        </div>
+        <!-- /.col (RIGHT) -->
       </div>
       <!-- /.row -->
 
