736df9af94e87808b5da153e6dd9a461433a0de4
jenyruth <giemi.7baf@gmail.com>
2019-01-09 09:44:31 -0500

    subiendo estudiante

--- CPU-UNAMBA/application/views/estudiante/index.php
+++ CPU-UNAMBA/application/views/estudiante/index.php
@@ -1,9 +1,9 @@
-<br>
+﻿<br>
 <br>
 <!--CUERPO-->
 <div class="container p-3">
         <div>
-            <h1 class="font-weight-bold text-center text-info">FICHA DE INSCRIPCION DEL POSTULANTE</h1>           
+            <h1 class="font-weight-bold text-center text-info"> probando FICHA DE INSCRIPCION DEL POSTULANTE</h1>           
         </div>
        <hr class="featurette-divider bg-info">
 
