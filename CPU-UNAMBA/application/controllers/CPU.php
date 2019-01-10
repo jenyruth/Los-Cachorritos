8fe50fdd7d3733feb35b1a237d6675dd60384214
Cachorritolinze <cachorritosubaru@gmail.com>
2019-01-09 22:25:56 -0500

    editando controlador CPU.php para envio de Id

--- CPU-UNAMBA/application/controllers/CPU.php
+++ CPU-UNAMBA/application/controllers/CPU.php
@@ -6,15 +6,24 @@ class CPU extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
-        //$this->load->model('Model_cpu');
+        $this->load->model('Model_cpu');
     }
 
     public function index()
     {
-	$this->load->view('layouts/head');
+    $this->load->view('layouts/head');
         $this->load->view('layouts/header_1');
         
-        $this->load->view("estudiante/index");
+
+        //pasando datos de los combos
+        $datos = [
+                'options_departamentos' => $this->Model_cpu-> selectdepartamentos()
+        ];
+
+        //finisehd
+
+
+        $this->load->view("admin/estudiante",$datos);
         $this->load->view('layouts/footer');
         
     }
