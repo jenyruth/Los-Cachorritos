0e16a15f474b1dfeea9d6f6f368f80824d337d00
Cachorritolinze <cachorritosubaru@gmail.com>
2019-01-09 22:13:20 -0500

    modificando la coneccion del combobox deprtamento

--- CPU-UNAMBA/application/models/Modelo_cpu.php
+++ CPU-UNAMBA/application/models/Modelo_cpu.php
@@ -3,30 +3,16 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Modelo_cpu extends CI_Model {
+    
     function __construct() {
         parent::__construct();
-        $this->load->database();
     }
     
+    //Lista todos los departamentos
     public function depar() {
-        $query=$this->db->query("SELECT * FROM departamentos");
-        return $query->result();
+        return $this->db
+        ->order_by('Departamento')
+        ->get('departamentos');
     }
-    public function prov() {
-        $ID=3;
-        $query=$this->db->query("SELECT * from departamentos INNER JOIN provincias WHERE departamentos.IdDepartamento='$ID' AND provincias.IdDepartamento= '$ID'");
-        return $query->result();
-    }
-    public function dist() {
-        $query=$this->db->query("SELECT * FROM distritos");
-        return $query->result();
-    }
-    public function fac() {
-        $query=$this->db->query("SELECT * FROM distritos");
-        return $query->result();
-    }
-    public function carr() {
-        $query=$this->db->query("SELECT * FROM distritos");
-        return $query->result();
-    }
+    
 }
