f669e8d1f4573bb128d064021f075b10840fd894
Cachorritolinze <cachorritosubaru@gmail.com>
2019-01-10 12:39:02 -0500

    Cambio de Modelo_cpu a Model_cpu

--- CPU-UNAMBA/application/models/Model_cpu.php
+++ CPU-UNAMBA/application/models/Model_cpu.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Modelo_cpu extends CI_Model {
+class Model_cpu extends CI_Model {
     
     function __construct() {
         parent::__construct();
