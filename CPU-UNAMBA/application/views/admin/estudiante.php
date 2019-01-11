fe3ccc5c3f598783293d1f8b81b4e299c2c17bf9
jenyruth <giemi.7baf@gmail.com>
2019-01-11 18:54:29 -0500

    cambiando direccion url

--- CPU-UNAMBA/application/views/admin/estudiante.php
+++ CPU-UNAMBA/application/views/admin/estudiante.php
@@ -1,4 +1,4 @@
-<br>
+﻿<br>
 <br>
 <!--CUERPO-->
 <div class="container p-3">
@@ -9,7 +9,7 @@
 
 
 <div class="container">
-    <form action="<?php echo base_url();?>mantenimiento/categorias/store" method="POST">
+    <form action="<?php echo base_url();?>registrarDatos" method="POST">
        <!--caso datos personales-->
        <fieldset>
                <legend>I. Datos Personales</legend>
