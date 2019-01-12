224a6e2cc9810d8e764f00d2151d89bca4b4b88d
johnel7 <nhelrojas00@gmail.com>
2019-01-12 07:10:29 -0500

    test01

--- CPU-UNAMBA/application/controllers/Funciones.php
+++ CPU-UNAMBA/application/controllers/Funciones.php
@@ -2,7 +2,7 @@
  defined('BASEPATH') OT exit ('no direccionar a ningun lado ');
  class Funciones extends CI_Controller
  {
- 	public funtion insertarDatos(){
+ 	public function insertarDatos(){
  		
  	}
  }
