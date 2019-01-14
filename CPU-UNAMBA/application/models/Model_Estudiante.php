11a27cbb57289972bc32e526363aa49b46a41a64
132090 <132090.rock@gmail.com>
2019-01-14 12:23:33 -0500

    tomando los datos y registrando a la tabla usuario

--- CPU-UNAMBA/application/models/Model_Estudiante.php
+++ CPU-UNAMBA/application/models/Model_Estudiante.php
@@ -9,6 +9,17 @@ class Model_Estudiante extends CI_Model {
     }
 
     public function Estudiane($nomPost,$apePaterno,$apeMaterno,$dni,$sexo,$domicilio,$correo,$telf,$fechaNac,$idColegio,$idCarrera,$idVoucher,$idDistrito){
-        
+        $array=array(
+            'nomPost'=>$nomPost,
+            'apePaterno'=>$apePaterno,
+            'apeMaterno'=>$apeMaterno,
+            'dni'=>$dni,
+            'sexo'=>$sexo,
+            'domicilio'=>$domicilio,
+            'correo'=>$correo,
+            'telf'=>$telf,
+            'fechaNac'=>$fechaNac
+        );
+        $this->db->insert('postulante');
     }
 }
