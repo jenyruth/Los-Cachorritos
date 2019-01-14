5771954e50e1331b230c2a12cbe0c517844ef4a3
132090 <132090.rock@gmail.com>
2019-01-14 12:11:07 -0500

    tomando los datos enviados

--- CPU-UNAMBA/application/models/Model_Estudiante.php
+++ CPU-UNAMBA/application/models/Model_Estudiante.php
@@ -2,13 +2,13 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Model_Inic extends CI_Model {
+class Model_Estudiante extends CI_Model {
     function __construct() {
         parent::__construct();
         $this->load->database();
     }
 
-    public function Estudiane() {
+    public function Estudiane($nomPost,$apePaterno,$apeMaterno,$dni,$sexo,$domicilio,$correo,$telf,$fechaNac,$idColegio,$idCarrera,$idVoucher,$idDistrito){
         
     }
 }
