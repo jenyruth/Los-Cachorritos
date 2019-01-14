d0eeebaf8b9a54f7fbe50f1f034d3bdf81a7cc1b
132090 <132090.rock@gmail.com>
2019-01-14 11:33:34 -0500

    conectando mi modelo con estudiante con la db

--- CPU-UNAMBA/application/models/Model_Estudiante.php
+++ CPU-UNAMBA/application/models/Model_Estudiante.php
@@ -3,7 +3,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Model_Inic extends CI_Model {
-    
+    function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+
     public function Estudiane() {
         
     }
