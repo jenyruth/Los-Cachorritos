9f84ec3d0704b71289f4ae1954263755266c6312
jenyruth <giemi.7baf@gmail.com>
2019-01-14 16:50:36 -0500

    borrando codigo innecesario

--- CPU-UNAMBA/application/controllers/Funciones.php
+++ CPU-UNAMBA/application/controllers/Funciones.php
@@ -36,27 +36,7 @@
         //Aca lo que hacemos es guardar en un array los datos que se reciben por el POST, procura que el nombre de las claves de tu array sea igual al nombre de los campos de tu tabla
 	
 
-		/*$identificador=this->db->insert_id()
-				
-				'apellidoMaterno'= $this->input->post("apellidoMaterno");
-				'nombres'= $this->input->post("nombre");
-				'sexo'= $this->input->post("");
-				'dni'= $this->input->post("numeroDocumento");
-				'domicilio'= $this->input->post("domicilio");
-				'edad'= $this->input->post("edad");
-				'fechaNac'= $this->input->post("fechaNac");
-				'departamento'= $this->input->post("departamento");
-				'provincia'= $this->input->post("provincia");
-				'distrito'= $this->input->post("distrito");
-				'nombreColegio'= $this->input->post("");
-				'departamentoColegio'= $this->input->post("departamento");
-				'provinciaColegio'= $this->input->post("provincia");
-				'distritoColegio'= $this->input->post("distrito");
-				'anioEgreso'= $this->input->post("anioEgreso");
-				'facultad'= $this->input->post("facultad");
-				'carreaProfesional'= $this->input->post("carreaProfesional");
-				$res = $this->Usuarios_model->login($username, 
-		*/
+		
 				//if ($this -> )
 		}
 }
