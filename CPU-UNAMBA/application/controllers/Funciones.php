17ed1e4a72a65ddb34c18ae0a328a7c2600c5142
132090 <132090.rock@gmail.com>
2019-01-14 13:08:01 -0500

    test de registro

--- CPU-UNAMBA/application/controllers/Funciones.php
+++ CPU-UNAMBA/application/controllers/Funciones.php
@@ -3,26 +3,26 @@
  class Funciones extends CI_Controller{
       public function __construct() {
         parent::__construct();
-        //$this->load->model("Model_carrera");
+        $this->load->model("Model_Estudiante");
     }
     public function insertarDatos(){
-        $datos= $this->imput->post();
+        $datos= $this->input->post();
         
         if(isset($datos)){
             
             $nomPost=$datos['txtNombres'];
             $apePaterno=$datos['txtapellpat'];
             $apeMaterno=$datos['txtapellmat'];
-            $dni=$datos['txtsexo'];
-            $sexo=$datos['txtdni'];
+            $dni=$datos['txtdni'];
+            $sexo=$datos['txtsexo'];
             $domicilio=$datos['txtdomicilio'];
             $correo=$datos['txtcorreo'];
             $telf=$datos['txtcelular'];
             $fechaNac=$datos['txtfechanac'];
-            $idColegio=$datos['txtcolegio'];
+            //$idColegio=$datos['txtcolegio'];
             //$txtprov=$datos['txtprov'];
-            $idCarrera=$datos['txtcarr'];
-            $idDistrito=$datos['txtdep'];
+           // $idCarrera=$datos['txtcarr'];
+            //$idDistrito=$datos['txtdep'];
             //$txtlugarnac=$datos['txtlugarnac'];
             //$txtdepartn=$datos['txtdepartn'];
             //$txtprovn=$datos['txtprovn'];
@@ -30,7 +30,7 @@
             //$txtanoe=$datos['txtanoe'];
             //$txtfacult=$datos['txtfacult'];
            // $txtcarr=$datos['txtcarr'];
-           $this->Model_Estudiante->Estudiane($nomPost,$apePaterno,$apeMaterno,$dni,$sexo,$domicilio,$correo,$telf,$fechaNac,$idColegio,$idCarrera,$idVoucher,$idDistrito);
+           $this->Model_Estudiante->Estudiante($nomPost,$apePaterno,$apeMaterno,$dni,$sexo,$domicilio,$correo,$telf,$fechaNac);
            redirect('');
         }
         //Aca lo que hacemos es guardar en un array los datos que se reciben por el POST, procura que el nombre de las claves de tu array sea igual al nombre de los campos de tu tabla
